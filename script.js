b4a130557c66c36cbc8b7f26cba06ce920d9f6c5
LucasYNL <michaelnggg@hotmail.com>
2023-06-15 00:15:48 -0700

    populate display

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+const display = document.querySelector(".display");
+const digits = document.querySelectorAll(".nums, .ops");
+const operators = document.querySelectorAll(".ops");
+const calculate = document.querySelector(".do");
+
+let screen1 = [];
+
 // BASIC MATH FUNCTIONS 
 function add(i, x) {
     return i + x;
@@ -38,3 +45,20 @@ function operate(num1, num2, op) {
 
     return result;
 }
+
+function populate(num) {
+    screen1.push(num);
+    display.value = screen1.join('');
+}
+
+digits.forEach(function (x) {
+    x.addEventListener('click', () => {
+        populate(x.value);
+    });
+})
+
+
+
+calculate.addEventListener('click', () => {
+    main();
+});
